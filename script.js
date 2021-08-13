149ad4f4c7fad22b8694c703a4e4bb82612a6107
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2021-08-13 18:09:03 -0300

    novo site

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+/* Realizando lucro/prejuízo com a venda de FIIs */
+
 function calcularLucro() {
 
 }
 
-function venda() {
+function resultado() {
     let precoMedio = document.getElementById('precoMedio').value
     let quantidadeDeCotas = document.getElementById('quantidadeDeCotas').value
     let valorDeVenda = document.getElementById('valorDeVenda').value
     imposto = (valorDeVenda - precoMedio) * 0.2
     darf = imposto * quantidadeDeCotas 
     lucro = ((valorDeVenda - precoMedio) * quantidadeDeCotas) - darf
+    prejuizo = ((valorDeVenda - precoMedio) * quantidadeDeCotas) * -1
 
     let res = document.querySelector('section#res')
 
@@ -17,6 +31,26 @@ function venda() {
     } else if (darf >= 10) {
         res.innerHTML = `<p>Seu lucro líquido na operação foi de R$${lucro.toFixed(2)} e você precisará emitir uma DARF no valor de R$${darf.toFixed(2)} até o último dia útil do mês seguinte à liquidação da operação que gerou o ganho.</p>`
     } else {
-        res.innerHTML = `<p>Seu prejuízo na operação foi de R$${lucro.toFixed(2)} e você poderá contabilizar esse prejuízo com outras vendas futuras para compensação.</p>`
+        res.innerHTML = `<p>Seu prejuízo na operação foi de R$${prejuizo.toFixed(2)} e você poderá contabilizar esse prejuízo com outras vendas futuras para compensação.</p>`
     }
+}
+
+/* Calculando Yield on Cost (YOC) de FIIs */
+
+function calcularYOC() {
+
+}
+
+function resultado2() {
+    let cmaquisicao = document.getElementById('cmaquisicao').value
+    cmaquisicao * 100
+    let rendimento = document.getElementById('rendimento').value
+    rendimento * 12
+    let yocost = rendimento / cmaquisicao * 100
+
+    let res2 = document.querySelector('section#res2')
+    let res3 = document.querySelector('section#res3')
+
+    res2.innerHTML = `<p>Seu yield on cost foi de ${yocost.toFixed(3)}% nesse mês.</p>`
+    res3.innerHTML = `<p>A rentabilidade anualizada seria ${(yocost * 12).toFixed(3)}%.</p>`
 }
